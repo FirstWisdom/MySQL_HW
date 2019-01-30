614200cce5a2e9d66368900ec530b92f0fd555e9
FirstWisdom <jason.zelaya21@yahoo.com>
2019-01-29 18:56:23 -0800

    all queries and some notes

--- MySQL_HW.sql
+++ MySQL_HW.sql
@@ -331,3 +331,24 @@ JOIN payment ON rental.rental_id=payment.rental_id
 GROUP BY name
 ORDER BY gross_revenue DESC
 LIMIT 5;
+
+-- 8a. In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. 
+-- Use the solution from the problem above to create a view.
+CREATE VIEW top_five_genres AS 
+SELECT name, SUM(amount) AS gross_revenue
+FROM category
+JOIN film_category ON category.category_id=film_category.category_id
+JOIN inventory ON film_category.film_id=inventory.film_id
+JOIN rental ON inventory.inventory_id=rental.inventory_id
+JOIN payment ON rental.rental_id=payment.rental_id
+GROUP BY name
+ORDER BY gross_revenue DESC
+LIMIT 5;
+
+-- 8b. How would you display the view that you created in 8a?
+SELECT * FROM top_five_genres;
+
+-- 8c. You find that you no longer need the view top_five_genres. Write a query to delete it.
+DROP VIEW top_five_genres;
+
+SELECT * FROM top_give_genres;
